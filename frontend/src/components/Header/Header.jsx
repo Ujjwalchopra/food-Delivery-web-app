360c1c9dd403790a0336f827d083fc11543f9364
Ujjwal <chopraujjwal222@gmail.com>
2024-05-17 09:17:55 +0530

    initial commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+export const Header = () => {
+  return (
+    <div className='header'>
+        <dev className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Order your favourite food here.Order your favourite food here.Order your favourite food here,Order your favourite food here</p>
+            <button>View Menu</button>
+        </dev>
+    </div>
+  )
+}
---
+++ frontend/src/context/StoreContext.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const StoreContext = () => {
+  return (
+    <div>StoreContext</div>
+  )
+}
