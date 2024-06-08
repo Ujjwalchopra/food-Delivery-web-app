813e34ff8a785fdde56db3e7432f370a498d3eb8
Ujjwalchopra <chopraujjwal222@gmail.com>
2024-06-08 16:19:22 +0530

    successfully completed the register user backend

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,52 +1,54 @@
-import {React,useEffect,useState} from 'react'
+import { React, useEffect, useState } from 'react'
 
 import './Add.css'
 import { assets } from '../../assets/assets'
 import axios from 'axios'
+import { toast } from 'react-toastify'
 
-  const Add = () => {
+const Add = ({url}) => {
 
-    const url= "http://localhost:4000"
- 
-    const [image,setImage]= useState(false);
-    const [data,setData]= useState({
-        name:"",
-        description:"",
-        price:"",
-        category:"salad"
+    
+
+    const [image, setImage] = useState(false);
+    const [data, setData] = useState({
+        name: "",
+        description: "",
+        price: "",
+        category: "salad"
     })
 
     const onChangeHandler = (event) => {
         const name = event.target.name;
         const value = event.target.value;
-        setData(data => ({...data,[name]:value}));
+        setData(data => ({ ...data, [name]: value }));
     }
 
-    const onSubmitHandler = async(event) => {
+    const onSubmitHandler = async (event) => {
         event.preventDefault();
-        const formData= new FormData();
-        formData.append("name",data.name);
-        formData.append("description",data.description);
-        formData.append("price",Number(data.price));
-        formData.append("category",data.category);
-        formData.append("image",image);
+        const formData = new FormData();
+        formData.append("name", data.name);
+        formData.append("description", data.description);
+        formData.append("price", Number(data.price));
+        formData.append("category", data.category);
+        formData.append("image", image);
 
-        const response= await axios.post(`${url}/api/food/add`,formData)
-        if(response.data.success){
+        const response = await axios.post(`${url}/api/food/add`, formData)
+        if (response.data.success) {
             setData({
-                name:"",
-                description:"",
-                price:"",
-                category:"salad"
+                name: "",
+                description: "",
+                price: "",
+                category: "salad"
             })
             setImage(false);
+            toast.success(response.data.message) // for adding notification after adding the data in backend successfully. 
         }
-        else{
-            
+        else {
+           toast.error(response.data.message);
         }
     }
 
-  
+
 
     return (
         <div className='add'>
@@ -54,17 +56,17 @@ import axios from 'axios'
                 <div className='add-img-upload flex-col'>
                     <p>Upload Image</p>
                     <label htmlFor="image">
-                        <img src={image?URL.createObjectURL(image):assets.upload_area} alt="" />
+                        <img src={image ? URL.createObjectURL(image) : assets.upload_area} alt="" />
                     </label>
                     <input onChange={(e) => setImage(e.target.files[0])} type="file" id='image' hidden required />
                 </div>
                 <div className="add-product-name flex-col">
                     <p>Product name</p>
-                    <input onChange={onChangeHandler} value ={data.name} type="text" name='name' placeholder='Type here' />
+                    <input onChange={onChangeHandler} value={data.name} type="text" name='name' placeholder='Type here' />
                 </div>
                 <div className="add-product-description">
                     <p>Product description</p>
-                    <textarea onChange={onChangeHandler} value ={data.description} name="description" rows="6" placeholder='write content here' required></textarea>
+                    <textarea onChange={onChangeHandler} value={data.description} name="description" rows="6" placeholder='write content here' required></textarea>
                 </div>
                 <div className="add-category-price">
                     <div className="add-category flex-col">
@@ -81,8 +83,8 @@ import axios from 'axios'
                         </select>
                     </div>
                     <div className="add-price flex-col">
-                      <p>Product price</p>
-                      <input onChange={onChangeHandler} value ={data.price} type="Number" name='price' placeholder='$20' />
+                        <p>Product price</p>
+                        <input onChange={onChangeHandler} value={data.price} type="Number" name='price' placeholder='$20' />
                     </div>
                 </div>
                 <button type='submit' className='add-btn'>ADD</button>
---
+++ backend/controllers/userController.js
@@ -0,0 +1,61 @@
+import userModel from "../models/userModels.js"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"; //for encrypt the password. 
+import validator from "validator"
+
+// login user
+const loginUser= async (req,res) => {
+
+}
+
+//create token
+
+ const createToken= (id) => {
+     return jwt.sign({id},process.env.JWT_SECRET);
+ }
+
+//register user
+const registerUser = async(req,res) => {
+    const {name,password,email} = req.body;
+    try {
+        //checking is user already exists;
+        const exists = await userModel.findOne({email});
+        if(exists){
+            return res.json({success: false,message: "user already exists"});
+        }
+
+        //validating email format and strong password
+       if(!validator.isEmail(email)){
+        return res.json({success: false,message: "Please enter a valid email"})
+       }
+
+       //checking password length
+       if(password.length<8){
+        return res.json({success:false,message: "Please enter a strong password of length more than 8"})
+       }
+      
+       //hashing user password
+       const salt= await bcrypt.genSalt(10) // here we encrypt our password of max length 10 we can encrypt this in between (5,10);
+       const hashedPassword = await bcrypt.hash(password,salt); 
+
+      //adding new user 
+      const newUser= new userModel({
+        name: name,
+        email:email,
+        password:hashedPassword
+      })
+      //save the user in db.
+      const user= await newUser.save();
+      
+      //sending the token as a response to user
+      const token= createToken(user._id);
+      res.json({success:true,token});
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+
+}
+
+export {loginUser,registerUser};
