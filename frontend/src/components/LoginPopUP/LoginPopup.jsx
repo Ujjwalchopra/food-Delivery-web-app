7afa460b58185dbf9e4096447a7bd57f770ddc01
Ujjwalchopra <chopraujjwal222@gmail.com>
2024-06-10 22:24:11 +0530

    login functionality is completed

--- frontend/src/components/LoginPopUP/LoginPopup.jsx
+++ frontend/src/components/LoginPopUP/LoginPopup.jsx
@@ -1,32 +1,82 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './LoginPopup.css'
 import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
+
 const LoginPopup = ({ setShowLogin }) => {
-    const [currState, setCurrState] = useState("Login in");
+    const [currState, setCurrState] = useState("Login");
+
+
+    const { url, setToken } = useContext(StoreContext);
+    //getting the login data from user
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data => ({ ...data, [name]: value }));
+    }
+
+    // useEffect(()=> {
+    //     console.log(data);
+    // },[data]);
+
+    const onLogin = async (event) => {
+        event.preventDefault();
+        let newurl = url;
+        if (currState === 'Login') {
+            newurl += "/api/user/login"
+        }
+        else {
+            newurl += "/api/user/register"
+        }
+
+        //calling the api
+        const response = await axios.post(newurl, data);
+
+        //saving the token
+        if (response.data.success) {
+            setToken(response.data.token)
+            localStorage.setItem("token", response.data.token);
+            setShowLogin(false);
+        }
+        else{
+            alert(response.data.message);
+        }
+
+
+
+    }
+
     return (
         <div className='login-popup'>
-            <form className="login-popup-container">
+            <form onSubmit={onLogin} className="login-popup-container">
                 <div className="login-popup-title">
                     <h2>{currState}</h2>
                     <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
                 </div>
                 <div className="login-popup-inputs">
-                    {currState === "Login in" ? <></> : <input type="text" placeholder='Your name' required />}
+                    {currState === "Login" ? <></> : <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='Your name' required />}
 
-                    <input type="email" placeholder='Your email' required />
-                    <input type="password" placeholder='Password' required />
+                    <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Your email' required />
+                    <input name='password' onChange={onChangeHandler} value={data.password} type="password" placeholder='Password' required />
                 </div>
-                <button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
+                <button type='submit'>{currState === "Sign Up" ? "Create account" : "Login"}</button>
                 <div className="login-popup-condition">
                     <input type="checkbox" required />
                     <p>By continuing, i agree to the terms of use & privacy policy.</p>
                 </div>
-                {currState==="Login in"
-                ?<p>Create a new account? <span onClick={()=> setCurrState("Sign Up")}>Click here</span></p>
-                :<p>Already have an account? <span onClick={()=> setCurrState("Login in")}>Login here</span></p>
-            }
-                
-                
+                {currState === "Login"
+                    ? <p>Create a new account? <span onClick={() => setCurrState("Sign Up")}>Click here</span></p>
+                    : <p>Already have an account? <span onClick={() => setCurrState("Login")}>Login here</span></p>
+                }
+
+
             </form>
         </div>
     )
