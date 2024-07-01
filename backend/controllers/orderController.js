5dd4862c4928d9d6b30dba490a17339e7b0cf6f0
Ujjwalchopra <chopraujjwal222@gmail.com>
2024-07-01 14:16:22 +0530

    all APIs are created succesfully

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,60 +5,112 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 //placing user order from frontend
-const placeOrder = async (req,res) => {
+const placeOrder = async (req, res) => {
 
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "http://localhost:5174";
 
     try {
-        const newOrder= new orderModel({
+        const newOrder = new orderModel({
             userId: req.body.userId,
-            items:req.body.items,
-            amount:req.body.amount,
-            address:req.body.address
+            items: req.body.items,
+            amount: req.body.amount,
+            address: req.body.address
         })
         await newOrder.save();
         //after placing an order we have to clear cartdata of that particular user
-        await userModel.findByIdAndUpdate(req.body.userId,{cartData: {}})
-        
+        await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} })
+
         //logic for genrating payment link using stripe. 
 
         const line_items = req.body.items.map((item) => ({
             price_data: {
                 currency: "inr",
                 product_data: {
-                    name:item.name
+                    name: item.name
                 },
-                unit_amount:item.price*100*80 //we get price in dollar so we have to convert it into rupee
+                unit_amount: item.price * 100 * 80 //we get price in dollar so we have to convert it into rupee
             },
-            quantity:item.quantity
+            quantity: item.quantity
         }))
         //adding delvery charges
         line_items.push({
-            price_data:{
-                currency:"inr",
-                product_data:{
-                    name:"Delivery Charges"
+            price_data: {
+                currency: "inr",
+                product_data: {
+                    name: "Delivery Charges"
                 },
-                unit_amount: 2*100*80
+                unit_amount: 2 * 100 * 80,
             },
-            quantity:1
+            quantity: 1
         })
 
         //for each lineitems we generate a session
         const session = await stripe.checkout.sessions.create({
-            line_items:line_items,
+            line_items: line_items,
             mode: 'payment',
             success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
             cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
         })
 
-        res.json({success:true,session_url:session.url})
+        res.json({ success: true, session_url: session.url })
 
     } catch (error) {
         console.log(error);
-        res.json({success:false,message:"Error"})
+        res.json({ success: false, message: "Error" })
     }
 
 }
 
-export {placeOrder};
+//function for verify page. 
+const verifyOrder = async (req, res) => {
+    const { success, orderId } = req.body;
+    try {
+        if (success == "true") {
+            await orderModel.findByIdAndUpdate(orderId, { payment: true });
+            res.json({ success: true, message: "Paid" });
+        }
+        else {
+            await orderModel.findByIdAndDelete(orderId);
+            res.json({ success: true, message: "Not Paid" });
+        }
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" })
+    }
+}
+
+//user orders for frontend
+const userOrders = async(req,res) => {
+    try {
+        const orders = await orderModel.find({userId:req.body.userId});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"});
+    }
+}
+//Listing orders for admin panel.
+const listOrders = async (req,res) => {
+    try {
+        const orders= await orderModel.find({});
+        res.json({success:true,data:orders});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"});
+    }
+}
+
+// api for updating order status
+
+const updateStatus = async (req,res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status})
+        res.json({success:true,message:"Status Updated"});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"});
+    }
+}
+
+export { placeOrder, verifyOrder,userOrders,listOrders,updateStatus};
