d05fa3467633b479c2ddf7132ef390bb3aac11eb
Ujjwalchopra <chopraujjwal222@gmail.com>
2024-07-01 14:13:32 +0530

    fullstack food delivery website is completed successfully as mentioned and working fine

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -2,7 +2,9 @@ import React, { useContext, useState } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext'
 import axios from 'axios';
-import { error } from 'console';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 
 export const PlaceOrder = () => {
 
@@ -63,6 +65,17 @@ export const PlaceOrder = () => {
       alert("error");
      }
   }
+   
+  const navigate= useNavigate();
+
+  useEffect(() => {
+     if(!token){
+        navigate('/cart')
+     }
+     else if(getTotalCartAmount() === 0){
+      navigate('/cart');
+     }
+  }, [token]);
 
 
   return (
